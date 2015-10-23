1a5ad9046a8273fad9a27648400917e3c92eae5d
Aaron Ran An <anran.pku@gmail.com>
2015-10-23 02:10:42 -0500

    adding lollipop chart for fandango movie data

--- Course Project/analysis_fandango.R
+++ Course Project/analysis_fandango.R
@@ -2,3 +2,58 @@ fan_score = read.csv('./Course Project/fandango_score_comparison.csv')
 
 fan_scrap = read.csv('./Course Project/fandango_scrape.csv')
 
+library(rCharts)
+
+library(ggplot2)
+
+library(reshape)
+
+# data pre-processing
+
+# reshape the data into a better format for plotting
+
+long_star = fan_score[,c(1, 7, 10, 12, 13)]
+
+as.character(long_star$FILM)
+
+as.character(long_melt$film)
+
+long_melt = melt(long_star, id.vars= "FILM")
+
+names(long_melt) = c("film", "website", "rating")
+
+ggplot(long_melt, aes(x=rating, fill=website)) + geom_density(alpha=.3)
+
+ggplot(long_melt, aes(x=website, y=rating, fill=website)) + geom_boxplot(alpha = .3)
+
+####
+
+library(rCharts)
+
+long_melt[,1:2] = apply(long_melt[,1:2], 2, function(x) iconv(x,to="utf-8"))
+
+d1 <- dPlot(
+        x = "rating",
+        y = "film",
+        groups = "website",
+        data = long_melt,
+        type = "bubble"
+        , bounds = list( x = 250, y = 50, width = 700, height = 2400)
+)
+
+d1$xAxis( type = "addMeasureAxis" )
+d1$yAxis( type = "addCategoryAxis", orderRule = "film" )
+d1$legend(
+        x = 200,
+        y = 10,
+        width = 300,
+        height = 200,
+        horizontalAlign = "left"
+)
+
+d1$set(height = 2500, width = 1000)
+
+
+d1
+
+
